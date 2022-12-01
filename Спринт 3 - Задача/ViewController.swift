2184b08899476fd31c804959be051dd5f19d9ef1
Andrey Nikolaev <Andreynick2006@ya.ru>
2022-12-01 23:09:18 +1200

    Добавил лэйбл отображающий счетчик нажатий  и кнопку которая фиксирует нажатия

--- Спринт 3 - Задача/ViewController.swift
+++ Спринт 3 - Задача/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var count: Int = 0
+    @IBOutlet weak var CountLabel: UILabel!
+    @IBOutlet weak var CountButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        CountLabel.text = "Значение счетчика: \(count)"
     }
 
-
+    @IBAction func CountButtonPress(_ sender: Any) {
+        count += 1
+        CountLabel.text = "Значение счетчика: \(count)"
+    }
+    
 }
 
